dbe41f52b11e9582f956eb1a9ce821a5ee7c7878
jiatongw <codewjt@gmail.com>
2018-09-11 21:37:52 -0700

    balanced array

--- balance_array.py
+++ balance_array.py
@@ -1,11 +1,7 @@
 
-l=[2,2]
-
-
 def balance(l):
     if len(l)==0 or len(l) == 1:
         return 0
-
     
     for i in range(1,len(l)):
         print(l[:l[i-1]])
@@ -15,5 +11,7 @@ def balance(l):
         
     return -1
 
+
+l=[2, 2]
 print(balance(l))
         
