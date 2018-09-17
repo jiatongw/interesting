86da3df9e2322ea5e3eba6a449be5329d32393d4
jiatongw <codewjt@gmail.com>
2018-09-17 16:57:41 -0700

    Using heap/priority queue

--- nlargest.py
+++ nlargest.py
@@ -1,7 +1,29 @@
-def n_largest(nums):
-    import heapq
-    return heapq.nlargest(3,nums)
+# def n_largest(nums):
+#     import heapq
+#     return heapq.nlargest(3,nums)
+#
+# if __name__ == "__main__":
+#     nums = [1,2,3,4,3,10,8,22]
+#     print(n_largest(nums))
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        import heapq
+        min_heap = [-float('inf')]*k
+        heapq.heapify(min_heap)
+        for num in nums:
+            if num > min_heap[0]:
+                heapq.heappop(min_heap)
+                heapq.heappush(min_heap,num)
+        return min_heap[0]
 
 if __name__ == "__main__":
     nums = [1,2,3,4,3,10,8,22]
-    print(n_largest(nums))
+    s=Solution()
+    print(s.findKthLargest(nums,3))
+
+
